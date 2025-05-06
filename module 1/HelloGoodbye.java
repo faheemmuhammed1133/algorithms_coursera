74f43a6942ef7dd6b416e98ed1c36e1e6d301a32
faheemmuhammed1133 <2023.muhammedf@isu.ac.in>
2025-05-06 23:23:37 +0530

    Module 1 created

---
+++ module 1/HelloGoodbye.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello "+args[0]+ "and "+args[1]);
+        System.out.println("Goodbye "+args[0]+ "and "+args[1]);
+    }
+
+}
